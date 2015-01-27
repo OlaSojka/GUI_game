b6b792904ef959bc34555c83f6540fc98dc40d09
OlaSojka <olasojka@gmail.com>
2015-01-27 09:57:50 +0100

    cd

---
+++ RTS Tutorial/Assets/WorldObject/Building/Barracks/Barracks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using RTS;
+
+public class Barracks : Building {
+	
+	protected override void Start () {
+		base.Start();
+		actions = new string[] { "Coś co robią baraki" };
+	}
+	
+	public override void PerformAction(string actionToPerform) {
+		base.PerformAction(actionToPerform);
+		CreateUnit(actionToPerform);
+	}
+	
+	protected override bool ShouldMakeDecision () {
+		return false;
+	}
+}
--- RTS Tutorial/Assets/WorldObject/Building/WarFactory/WarFactory.cs
+++ RTS Tutorial/Assets/WorldObject/Building/WarFactory/WarFactory.cs
@@ -5,7 +5,7 @@ public class WarFactory : Building {
 
 	protected override void Start () {
 		base.Start();
-		actions = new string[] { "Tank", "Tank2" };
+		actions = new string[] { "Tank" };
 	}
 	
 	public override void PerformAction(string actionToPerform) {
